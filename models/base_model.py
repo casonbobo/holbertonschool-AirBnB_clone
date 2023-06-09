c59652e452fda558b8db813e8594e987f97fc6d2
root <Caramonh@yahoo.com>
2023-06-09 11:44:14 -0700

    4

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,18 @@ from datetime import datetime
 
 class BaseModel():
     """Base Model Class"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            if '__class__' in kwargs:
+                kwargs.pop('__class__')
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Saves and updates the date"""
