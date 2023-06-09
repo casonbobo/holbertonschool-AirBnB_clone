2ca1be1cadc1395095d00931c46eb2259343fe31
casonbobo <casonsbobo@gmail.com>
2023-06-09 13:45:36 -0700

    i think its the last push of the day

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
     for all classes for the AirBnB project"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
@@ -18,7 +19,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """Saves and updates the date"""
