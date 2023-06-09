96a34bc630e758d42a50b19390c348b343803778
casonbobo <casonsbobo@gmail.com>
2023-06-09 13:48:03 -0700

    somabutch

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
 
     def save(self):
         """Saves and updates the date"""
