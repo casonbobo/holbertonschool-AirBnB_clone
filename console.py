1dede3d8fc5b3249ecaa66d21b6a15ab8e9d9eae
root <Caramonh@yahoo.com>
2023-06-13 11:38:51 -0700

    console user fix

--- console.py
+++ console.py
@@ -7,6 +7,12 @@ import models
 import shlex
 from models.base_model import BaseModel
 from models.models import model_classes
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
